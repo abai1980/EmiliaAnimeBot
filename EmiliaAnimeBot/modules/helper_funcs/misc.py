7e51d263318a919775e289c68aa3c5ecc4d1c3c2
abai1980 <88306838+abai1980@users.noreply.github.com>
2021-09-18 13:43:28 +0530

    Update misc.py

--- EmiliaAnimeBot/modules/helper_funcs/misc.py
+++ EmiliaAnimeBot/modules/helper_funcs/misc.py
@@ -74,9 +74,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("🔺", callback_data="{}_prev({})".format(prefix, modulo_page)),
+            (EqInlineKeyboardButton("🔼", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("Back", callback_data="asuna_back"),
-             EqInlineKeyboardButton("🔻", callback_data="{}_next({})".format(prefix, modulo_page)))]
+             EqInlineKeyboardButton("🔽", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("Back", callback_data="info_back")]]
